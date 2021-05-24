a67bb876a3e7c4219c388cd2b504ef23a1d8a8d6
Bastian Diaz <basty.diaaz@gmail.com>
2021-05-24 05:07:18 -0400

    :sparkles: caracteristica: tooltips para cards en highlights section

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// Habilitar tooltips
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+const tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));

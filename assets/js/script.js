bab5c152c66ea1d04eb7b6495d44e624cada62b7
Bastian Diaz <basty.diaaz@gmail.com>
2021-05-24 05:17:45 -0400

    :sparkles: caracteristica: click en cards para contraer/colapsar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 // Habilitar tooltips
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 const tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+// Contraer / colapsar cards
+$('.card').on('click', (e) => {
+  $('.card-body').toggle();
+});
